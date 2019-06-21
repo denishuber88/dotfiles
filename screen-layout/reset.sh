58a64932e0a2979067aa1746b81616b58c0428aa
Denis Huber <denis.huber@qaware.de>
2019-06-21 22:38:23 +0200

    Add screen layout script for 3 monitors

---
+++ screen-layout/reset.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+xrandr --output DP-1-1-1 --off --output DP-1-1-2 --off
---
+++ screen-layout/work.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+xrandr --output DP-1-1-2 --auto --left-of eDP1 --output DP-1-1-1 --auto --left-of DP-1-1-2
+
